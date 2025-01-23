4ee8257254d2bec18225b4d869ff0501edad6b42
Arun Paul <arunpaul@2hats.com>
2025-01-23 13:11:36 +0530

    search accessing using keyboard functionaliity implimented and wrapped search input with form tag

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const radioButtons = document.querySelectorAll('input[name="color"]');
-    const displayText = document.querySelector('#color-value');
-  
-    radioButtons.forEach((radio) => {
-      radio.addEventListener('change', (event) => {
-        displayText.textContent = `${event.target.value}`;
-      });
+  const radioButtons = document.querySelectorAll('input[name="color"]');
+  const displayText = document.querySelector('#color-value');
+
+  radioButtons.forEach((radio) => {
+    radio.addEventListener('change', (event) => {
+      displayText.textContent = `${event.target.value}`;
     });
   });
+});
 
-  console.log("script worked")
+document.addEventListener('keydown', (event) => {
+  // Check for the desired key combination (Ctrl + Shift + F)
+  console.log('event detected')
+
+  if (event.ctrlKey && event.shiftKey && event.key === 'F') {
+    console.log('event detected')
+    event.preventDefault(); // Prevent any default browser action
+    const searchInput = document.getElementById('search');
+    searchInput.focus(); // Focus on the search input
+    searchInput.select(); // Optionally, select existing text
+  }
+});
+
+console.log("script worked")
